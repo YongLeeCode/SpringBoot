c482bf029df57c49c625e2f1e4e63da80548e057
YongLeeCode <yonghwan311@gmail.com>
2025-01-25 20:07:26 +0900

    feat: 스프링 부트 스타터 웹 추가

--- src/main/java/me/yong/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/yong/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,4 +1,12 @@
 package me.yong.springbootdeveloper;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//build.gradle파일의 dependencies에 spring-boot-starter-web 추가해야함
+@SpringBootApplication
 public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
 }
